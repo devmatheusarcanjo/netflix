91d2a4a411551a35d6ef350ee00dfda94a5389ef
Matheus Souza <matheussouzaarcanjo444@gmail.com>
2024-11-18 15:34:38 -0300

    Layout adaptado para continuar em tamanho mobile mesmo em telas maiores

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,48 @@
-import { useState, useRef, useEffect} from 'react';
-import LayoutPadrao from "./templates/LayoutDefault.jsx"
-import { FaSearch, FaDownload } from "./components/icons/Icons.jsx";
-import Home from "@/pages/Home.jsx"
-import styles from './App.module.css'
-import useTouchHover, {styleHover} from "@/hooks/useTouchHover.js";
-
+import { useState, useRef, useEffect } from 'react';
+import LayoutPadrao from './templates/LayoutDefault.jsx';
+import { FaSearch, FaDownload } from './components/icons/Icons.jsx';
+import Home from '@/pages/Home.jsx';
+import styles from './App.module.css';
+import useTouchHover, { styleHover } from '@/hooks/useTouchHover.js';
 
 function App() {
-const [dadosHeader, setDadosHeader] = useState({
+  const [dadosHeader, setDadosHeader] = useState({
     comLogo: true,
-    titulo: "Minha conta",
-    icones: [FaSearch, FaDownload]
-})
+    titulo: 'Minha conta',
+    icones: [FaSearch, FaDownload],
+  });
 
-    const content = useRef(null)
-    useTouchHover(content)
+  const content = useRef(null);
+  useTouchHover(content);
 
-
-
-// ABRIR EM TELA CHEIA
-useEffect(() => {
-
-    function full() {                
-        content.current.requestFullscreen()
+  // ABRIR EM TELA CHEIA
+  useEffect(() => {
+    function full() {
+      content.current
+        .requestFullscreen()
         .then(console.log)
-        .catch(console.error)   
+        .catch(console.error);
     }
 
-    window.addEventListener("click", (event) => {
-        console.log("foi")
-        full()        
-    }, {once: true})
-    
-},[content])
+    window.addEventListener(
+      'click',
+      (event) => {
+        console.log('foi');
+        full();
+      },
+      { once: true }
+    );
+  }, [content]);
 
-
-    
-  
   return (
-    <div className={`${styles.container} ${styleHover.global}`} ref={content}>
-      <LayoutPadrao dadosHeader={dadosHeader}>
+    <div className={styles.containerCenter} ref={content}>
+      <div className={`${styles.container} ${styleHover.global}`}>
+        <LayoutPadrao dadosHeader={dadosHeader}>
           <Home />
-      </LayoutPadrao>
+        </LayoutPadrao>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/popups/popupAlertResponsiveness.jsx
@@ -0,0 +1,3 @@
+export default function popupAlertResponsiveness() {
+  return <div></div>;
+}
