8c4810ab850439c6532a2ee1bf2a407cf1d1c765
Matheus Souza <matheussouzaarcanjo444@gmail.com>
2024-11-18 19:38:24 -0300

    Lazy loading implementado nas imagens da listagem de filmes

--- src/components/movies/MovieItem.jsx
+++ src/components/movies/MovieItem.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import styles from "./css/MovieItem.module.css"
-import { pathImage } from "@/utils/createPathToTMDB.js"
+import React from 'react';
+import styles from './css/MovieItem.module.css';
+import { pathImage } from '@/utils/createPathToTMDB.js';
+import LazyLoad from 'react-lazyload';
 
+export default function MovieItem({ data, key }) {
+  // Função para retornar a url absoluta de uma imagem da api do TMDB
+  const urlImage = pathImage({
+    filePath: data['poster_path'],
+  });
 
-export default function MovieItem({data, key}) {
-    // Função para retornar a url absoluta de uma imagem da api do TMDB 
-    const urlImage = pathImage({
-        filePath: data["poster_path"]
-    })
-    
-    // className={styles.over}
-    //         <div  data-item="hover" data-hover="black-05"></div>
-    
-    return (
-    <div className={`${styles.item}`} data-item="with-hover" key={data.id}>       
-        <img src={urlImage}/>
+  // className={styles.over}
+  //         <div  data-item="hover" data-hover="black-05"></div>
+
+  return (
+    <div className={`${styles.item}`} data-item="with-hover" key={data.id}>
+      <img src={urlImage} loading="lazy" />
     </div>
-    )
+  );
 }
